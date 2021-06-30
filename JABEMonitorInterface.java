2f5df13d8402ecc55db24acc22ea2af9203fb38b
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-06-30 20:10:54 +0200

    update

--- JABEMonitorInterface.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface JABEMonitorInterface extends Remote{
-    public void alertOnHigherBid(String ItemID)throws RemoteException;
-}
