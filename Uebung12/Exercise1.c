8fa4030eadd245b8b2fca18bac093e16cd208c18
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-07-17 14:37:40 +0200

    updated Exercise1.c :
    included operating time

--- Uebung12/Exercise1.c
+++ Uebung12/Exercise1.c
@@ -3,7 +3,6 @@
 int main(int argc, char *argv[])
 {
     int nProcesses, myRank, stag = 1;
-    MPI_Status status;
     MPI_Comm comm;
 
     comm = MPI_COMM_WORLD;
@@ -13,18 +12,38 @@ int main(int argc, char *argv[])
 
 
     if(nProcesses >= 2){
-        if(myRank == 0){
-            //root process
-            int message[10], size = 10, dest = 1;
-            MPI_Ssend(&message,size,MPI_INT,dest,stag,comm);
-            printf("%d : Successfully send!\n",myRank);
-            
-
-        }
-        else if(myRank == 1){
-            int message[10], size = 10, source = 0;
-            MPI_Recv(&message,size,MPI_INT,source,stag,comm,&status);
-            printf("%d : Successfully recieved!\n",myRank);
+        if(myRank == 1 || myRank == 0) {
+            double stime = MPI_Wtime();
+            int work = 1;
+            while(work) {
+                if(myRank == 0){
+                    //root process
+                    MPI_Status status;
+                    int message[10], size = 10, dest = 1, source =1;
+                    MPI_Ssend(&message,size,MPI_INT,dest,stag,comm);
+                    printf("%d : Successfully send!\n",myRank);
+                    fflush(stdout);
+                    MPI_Recv(&message,size,MPI_INT,source,stag,comm,&status);
+                    printf("%d : Successfully recieved!\n",myRank);
+                    fflush(stdout);
+        
+                }
+                else if(myRank == 1){
+                    MPI_Status status;
+                    int message[10], size = 10, source = 0, dest =0;
+                    MPI_Recv(&message,size,MPI_INT,source,stag,comm,&status);
+                    printf("%d : Successfully recieved!\n",myRank);
+                    fflush(stdout);
+                    MPI_Ssend(&message,size,MPI_INT,dest,stag,comm);
+                    printf("%d : Successfully send!\n",myRank);
+                    fflush(stdout);
+                }
+                if(MPI_Wtime() > stime + 10){
+                    work =0;
+                    MPI_Finalize();
+                    return 0;
+                }
+            }
         }
     }else{
         printf("2 or more Processes required!\n");
