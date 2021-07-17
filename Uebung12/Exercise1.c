7e40ef865d6880466c98cf31676960b58d4f27e6
GolawskyM <mario712000@yahoo.de>
2021-07-17 21:48:38 +0200

    Added MultiSend and changed sendcount to 1

--- Uebung12/Exercise1.c
+++ Uebung12/Exercise1.c
@@ -12,6 +12,8 @@ void Test(int type, int size, int iterations, int myRank) {
     if (myRank == 1 || myRank == 0){
         if (myRank == 0){
             // note Message start time
+            int bsize = size*sizeof(int) + MPI_BSEND_OVERHEAD;
+            char* bufptr = malloc(bsize);
             double tTotalElapsed = 0;
             double tTotalStart = MPI_Wtime();
             MPI_Status status;
@@ -26,15 +28,20 @@ void Test(int type, int size, int iterations, int myRank) {
                 double tMessageStart = MPI_Wtime();
                 // work...
                 if(type == 0)
-                    MPI_Send(message, size, MPI_INT, dest, stag, comm);
+                    MPI_Send(message, 1, MPI_INT, dest, stag, comm);
                 else if(type == 1)
-                    MPI_Ssend(message, size, MPI_INT, dest, stag, comm);
+                    MPI_Ssend(message, 1, MPI_INT, dest, stag, comm);
                 else if(type == 2) {
-                    MPI_Buffer_attach(&message, size);
-                    MPI_Bsend(message, size, MPI_INT, dest, stag, comm);
+                    MPI_Buffer_attach(bufptr, size + MPI_BSEND_OVERHEAD);
+                    //printf("Got so far\n");
+                    fflush(stdout);
+                    MPI_Bsend(message, 1, MPI_INT, dest, stag, comm);
+                    //printf("Got so far as well\n");
+                    fflush(stdout);
+                    MPI_Buffer_detach(&bufptr, &bsize);
                 }
                 //printf("%d : Successfully send!\n", myRank);
-                MPI_Recv(message, size, MPI_INT, source, stag, comm, &status);
+                MPI_Recv(message, 1, MPI_INT, source, stag, comm, &status);
                 //printf("%d : Successfully recieved!\n", myRank);
                 // note end time
                 double tMessageElapsed = MPI_Wtime() - tMessageStart;
@@ -53,8 +60,8 @@ void Test(int type, int size, int iterations, int myRank) {
                 }
             };
             tTotalElapsed = MPI_Wtime() - tTotalStart;
-            double byteSize = size * sizeof(int);
-            printf("Size (bytes): %lf\n",byteSize);
+            int byteSize = size * sizeof(int);
+            printf("Size (bytes): %d\n",byteSize);
             printf("Num of Iterations : %d\n",iterations);
             printf("Total Time elapsed (in Seconds): %lf\n",tTotalElapsed);
             double avgMessageTime =0;
@@ -66,20 +73,23 @@ void Test(int type, int size, int iterations, int myRank) {
             printf("\n\n");
         }
         else if (myRank == 1){
+            int bsize = size*sizeof(int) + MPI_BSEND_OVERHEAD;
+            char* bufptr = malloc(bsize);
             MPI_Status status;
             int source = 0, dest = 0;
             int *message;
             message = (int *)malloc(size * sizeof(int));
             while(work) {
-                MPI_Recv(message, size, MPI_INT, source, stag, comm, &status);
+                MPI_Recv(message, 1, MPI_INT, source, stag, comm, &status);
                 //printf("%d : Successfully recieved!\n", myRank);
                 if(type == 0)
-                    MPI_Send(message, size, MPI_INT, dest, stag, comm);
+                    MPI_Send(message, 1, MPI_INT, dest, stag, comm);
                 else if(type == 1)
-                    MPI_Ssend(message, size, MPI_INT, dest, stag, comm);
+                    MPI_Ssend(message, 1, MPI_INT, dest, stag, comm);
                 else if(type == 2) {
-                    MPI_Buffer_attach(&message, size);
-                    MPI_Bsend(message, size, MPI_INT, dest, stag, comm);
+                    MPI_Buffer_attach(bufptr, size + MPI_BSEND_OVERHEAD);
+                    MPI_Bsend(message, 1, MPI_INT, dest, stag, comm);
+                    MPI_Buffer_detach(&bufptr, &bsize);
                 }
                 //printf("%d : Successfully send!\n", myRank);
                 iteration += 1;
@@ -90,8 +100,63 @@ void Test(int type, int size, int iterations, int myRank) {
         }
 
     }
+}
 
+void MultiTest(int size, int iterations, int myRank)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Status status;
+    int n;
+    int iteration = 0;
+    int work = 1;
+    MPI_Comm_size(comm, &n);
+    MPI_Barrier(comm);
+    double startTime = MPI_Wtime();
+    double totalTime = 0;
 
+    int* message = (int*) malloc(size * sizeof(int));
+    int stag = 1;
+
+    if(myRank == 0) {
+        printf("MultiTest with %d Processes:\n", n);
+        fflush(stdout);
+        int messageTimes[iterations];
+        while(work) {
+            int i;
+            for(i = 1; i < n; ++i)
+                MPI_Send(message, 1, MPI_INT, i, stag, comm);
+            for(i = 1; i < n; ++i)
+                MPI_Recv(message, 1, MPI_INT, i, stag, comm, &status);
+            ++iteration;
+            if(iteration == iterations)
+                work = 0;
+            double elapsedTime = MPI_Wtime() - startTime;
+            startTime = elapsedTime;
+            totalTime += elapsedTime;
+            messageTimes[iteration] = elapsedTime;
+        }
+        //print out result of Test
+        double avgMessageTime = 0;
+        int i;
+        for(i = 0; i < iterations; ++i)
+            avgMessageTime += messageTimes[i];
+        avgMessageTime /= iterations;
+
+        printf("Size (bytes): %d\n", size*sizeof(int));
+        printf("Num of Iterations : %d\n", iterations);
+        printf("Total Time elapsed (in Seconds): %lf\n", totalTime);
+        printf("Average Message Time (in Seconds): %lf\n", avgMessageTime);
+        printf("\n\n");
+    }
+    else if(myRank > 0 && myRank < n){
+        while(work) {
+            MPI_Recv(message, 1, MPI_INT, 0, stag, comm, &status);
+            MPI_Send(message, 1, MPI_INT, 0, stag, comm);
+            ++iterations;
+            if(iterations == iteration)
+                work = 0;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -99,7 +164,7 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
 
     //Set up environment:
-    int nProcesses, myRank, iterations = 1000;
+    int nProcesses, myRank, iterations = 10000;
     MPI_Comm comm;
     // use world comm
     comm = MPI_COMM_WORLD;
@@ -112,12 +177,16 @@ int main(int argc, char *argv[])
     if(nProcesses >= 2)
     {
 
-        int type = 0;   //0: send, 1: synchronized send, 2: buffered send
+        int type = 2;   //0: send, 1: synchronized send, 2: buffered send, 3: send to many
         int sizes[] = {1, 5, 10, 100, 1000, 2000, 5000, 10000, 12000, 50000, 100000, 200000, 300000, 500000, 800000};
         int length = sizeof(sizes)/sizeof(int);
         int i;
-        for(i = 0; i < length; ++i)
-            Test(type, sizes[i], iterations, myRank);
+        if(type > -1 && type < 3)
+            for(i = 0; i < length; ++i)
+                Test(type, sizes[i], iterations, myRank);
+        else if(type == 3)
+            for(i = 0; i < length; ++i)
+                MultiTest(sizes[i], iterations, myRank);
 
     }
     else
